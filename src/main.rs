43b334783834d6f3532619053dd6e7f35f6a8b3d
Even Garli <127040541+evengarli@users.noreply.github.com>
2026-01-19 04:53:31 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use xeo::Xeo;
+use std::env;
+use xeo::Xeo; 
 
 fn main() {
-    let mut app = Xeo::new(80,"127.0.0.1".to_string());
-
+   
+    let port_str = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let port: u16 = port_str.parse().expect("PORT must be a number");
+    let mut app = Xeo::new(port, "0.0.0.0".to_string());
     app.route("/".to_string(), "index.html".to_string());
-
-    app.server("".to_string());
-
+    app.route("/docs".to_string(), "docs.html".to_string());
+    app.server();
 }
